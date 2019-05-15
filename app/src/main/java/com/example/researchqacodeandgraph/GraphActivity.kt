fd0dfee21328dc4ee31702f6ac2aa3003dc52642
trieuvd <trieuvu.mor@gmail.com>
2019-05-15 15:01:20 +0700

    research graph

---
+++ app/src/main/java/com/example/researchqacodeandgraph/GraphActivity.kt
@@ -0,0 +1,83 @@
+package com.example.researchqacodeandgraph
+
+import android.graphics.Color
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import kotlinx.android.synthetic.main.graph_activity.*
+
+class GraphActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.graph_activity)
+        customViewGraph()
+    }
+
+    private fun customViewGraph() {
+        lcRevenue.apply {
+            // background color
+            setBackgroundColor(Color.WHITE)
+
+            // disable description text
+            description.isEnabled = false
+
+            // enable touch gestures
+            setTouchEnabled(true)
+
+            //disable show grid
+            setDrawGridBackground(false)
+            xAxis.isEnabled = false
+
+            // enable scaling and dragging
+            isDragEnabled = true
+            setScaleEnabled(true)
+
+            //hide dual axis
+            axisLeft.isEnabled = false
+            axisRight.isEnabled = false
+
+            //hide legend
+            legend.isEnabled = false
+        }
+
+        setDataGraph(5, 10f)
+    }
+
+    /**
+     * xAxis is count
+     * yAxis is range
+     */
+    private fun setDataGraph(count: Int, range: Float) {
+        val values = mutableListOf<Entry>()
+
+        for (i in 0..count) {
+            val value = (Math.random() * range).toFloat()
+            values.add(Entry(i.toFloat(), value))
+        }
+
+        // create a dataset and give it a type
+        val lineDataSet = LineDataSet(values, "Dataset demo").apply {
+            setDrawIcons(false)
+            // black lines and points
+            color = Color.BLACK
+            setCircleColor(Color.BLACK)
+
+            //line thickness and point size
+            lineWidth = 1.5f
+            circleRadius = 6f
+
+            // set circle hole for point
+            setDrawCircleHole(true)
+            circleHoleRadius = 5f
+        }
+
+        // create a data object with the data sets
+        val data = LineData(lineDataSet)
+
+        // set data
+        lcRevenue.data = data
+    }
+}
---
+++ app/src/main/java/com/example/researchqacodeandgraph/QRCodeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.researchqacodeandgraph
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class QRCodeActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.qr_code_acitivity)
+    }
+}
